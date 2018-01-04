5f15fbf133b2be85e2c906baa540e707b2658d14
Max <m.nottret@hotmail.fr>
2018-01-04 16:29:51 +0100

    fix PMANAGER authorization bug

--- src/main/java/fr/lfml/manaproject/security/AuthoritiesConstants.java
+++ src/main/java/fr/lfml/manaproject/security/AuthoritiesConstants.java
@@ -5,14 +5,14 @@ package fr.lfml.manaproject.security;
  */
 public final class AuthoritiesConstants {
 
-    public static final String ADMIN = "ROLE_ADMIN";
+	public static final String ADMIN = "ROLE_ADMIN";
 
-    public static final String USER = "ROLE_USER";
+	public static final String USER = "ROLE_USER";
 
-    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+	public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
-    public static final String PMANAGER = "ROLE_PMANAGER"
+	public static final String PMANAGER = "ROLE_PMANAGER";
 
-    private AuthoritiesConstants() {
-    }
+	private AuthoritiesConstants() {
+	}
 }
